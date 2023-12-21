8cbe0d35efb0d3f4f6180766b6a47082d25d55f5
Daniel Gordillo <dgordilloguillen@gmail.com>
2023-12-21 12:21:00 +0100

    Funcionalidad completada con Spring Boot Admin

---
+++ infraestructuradomain/EurekaServer/src/main/java/com/paymentchain/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.paymentchain.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer //TEsto hace que este microservicio actue como un servidor Eureka
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+        
+
+
+}
